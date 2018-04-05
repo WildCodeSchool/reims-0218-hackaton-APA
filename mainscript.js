aed28b54317d6f2cb51650e8cfa968f3e9b72656
Aurelie Bayre <a.bayre@gmail.com>
2018-04-05 17:20:02 +0200

    feat: ADD random opponent
    create random number function from 0 to 1 to pick 1 opponent from the list

--- mainscript.js
+++ mainscript.js
@@ -1,10 +1,14 @@
+const randomOpponent = Math.round(Math.random() * 570) +1
+
+let opponent = {}
 const callMyLink = () => {
-  const url = `https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/` + "${functionRandom}"
+  const url = `https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/${randomOpponent}.json`
 
   fetch(url)
     .then(reponse => {
-      return reponse.json();
+      return reponse.json()
     })
-    .then(result => console.log(result));
-};
+    .then(result => opponent = result)
+}
 callMyLink();
+console.log(opponent)
