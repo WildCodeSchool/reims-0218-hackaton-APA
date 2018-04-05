0509f069540ab41e039f5698376be5ce0918b2f8
Pierre <dumaypierre@outlook.fr>
2018-04-05 19:49:04 +0200

    indent mainjs

--- mainscript.js
+++ mainscript.js
@@ -1,77 +1,101 @@
-const randomOpponent = Math.round(Math.random() * 570) +1
+const randomOpponent = Math.round(Math.random() * 570) + 1;
 
 //Définition des classes
 //Class Player
 class Player {
-  constructor (intelligence, strength, speed, durability, power, combat) {
-  this.lifePoints = 100
-  this.stats = {
+  constructor(intelligence, strength, speed, durability, power, combat) {
+    this.lifePoints = 100;
+    this.stats = {
       intelligence: intelligence,
       strength: strength,
       speed: speed,
       durability: durability,
       power: power,
       combat: combat
-    }
+    };
   }
   updateLifePoint(blow) {
-      this.lifePoints -= blow
-    }
+    this.lifePoints -= blow;
+  }
   addChance(ability) {
-    console.log("original: ", this.stats.power)
-    console.log("ability?", ability, this.stats[ability])
-      this.stats.power += Math.round(Math.random() * this.stats[ability])
+    console.log("original: ", this.stats.power);
+    console.log("ability?", ability, this.stats[ability]);
+    this.stats.power += Math.round(Math.random() * this.stats[ability]);
   }
 }
 //class HumanPlayer
 class HumanPlayer extends Player {
-  constructor (intelligence, strength, speed, durability, power, combat, name, battleCry) {
-    super(intelligence, strength, speed, durability, power, combat)
-    this.name = name,
-    this.battleCry = battleCry
+  constructor(
+    intelligence,
+    strength,
+    speed,
+    durability,
+    power,
+    combat,
+    name,
+    battleCry
+  ) {
+    super(intelligence, strength, speed, durability, power, combat);
+    (this.name = name), (this.battleCry = battleCry);
   }
 }
 //class ComputerPlayer
 class ComputerPlayer extends Player {
-  constructor (intelligence, strength, speed, durability, power, combat, name, battleCry) {
-    super(intelligence, strength, speed, durability, power, combat)
-    this.name = name
+  constructor(
+    intelligence,
+    strength,
+    speed,
+    durability,
+    power,
+    combat,
+    name,
+    battleCry
+  ) {
+    super(intelligence, strength, speed, durability, power, combat);
+    this.name = name;
   }
 }
 
-//Appel à l'API 
+//Appel à l'API
 const callMyLink = () => {
-  const url = `https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/3.json`
+  const url = `https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/3.json`;
 
   fetch(url)
     .then(reponse => {
-      return reponse.json()
+      return reponse.json();
     })
     .then(result => {
-      console.log(result)
-      const opponentStats = result.powerstats
+      console.log(result);
+      const opponentStats = result.powerstats;
 
-      console.log(opponentStats)
+      console.log(opponentStats);
       //const opponent = opponentStats.reduce(((acc, carry) =>  acc + carry), '')
-      let opponent = ''
-      const keysOpponent = Object.keys(opponentStats)
-      for (let key of keysOpponent){
-          opponent += `<li>${key} : ${opponentStats[key]}</li>`
+      let opponent = "";
+      const keysOpponent = Object.keys(opponentStats);
+      for (let key of keysOpponent) {
+        opponent += `<li>${key} : ${opponentStats[key]}</li>`;
       }
       //console.log(opponent)
-      const displayOpponent = document.getElementById("opponentCard")
-      displayOpponent.innerHTML = opponent
+      const displayOpponent = document.getElementById("opponentCard");
+      displayOpponent.innerHTML = opponent;
 
-      let human = ''
-      const playerHuman = new Player("Hubert de Montmirail", 34, 66, 55, 74, "Monjoie! Saint Denis!")
-      const keysHuman = Object.keys(playerHuman.stats)
-      for (let key of keysHuman){
-          human += `<li>${key} : ${playerHuman.stats[key]}</li>`
+      let human = "";
+      const playerHuman = new Player(
+        "Hubert de Montmirail",
+        34,
+        66,
+        55,
+        74,
+        "Monjoie! Saint Denis!"
+      );
+      const keysHuman = Object.keys(playerHuman.stats);
+      for (let key of keysHuman) {
+        human += `<li>${key} : ${playerHuman.stats[key]}</li>`;
       }
-      const displayHuman = document.getElementById("humanCard")
-      displayHuman.innerHTML = human
+      const displayHuman = document.getElementById("humanCard");
+      displayHuman.innerHTML = human;
 
-      //la boucle des différents tours 
-    })
-}
-callMyLink()
+      //la boucle des différents tours
+    });
+};
+callMyLink();
