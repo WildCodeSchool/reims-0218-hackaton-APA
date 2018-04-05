0d942bf27efa535484f25bb44172148a6a5c1aa8
Aurelie Bayre <a.bayre@gmail.com>
2018-04-06 01:39:45 +0200

    feat: ADD form

--- public/app.js
+++ public/app.js
@@ -7,8 +7,52 @@ const render = html => {
 const controllers = {
   '/': () => {
     console.log("je suis dans la route/")
-    const test = '<p>Je me choisis des supers caractérisitique!</p><a class="btn btn-success btn-lg" href="/opponent" role="button">See Opponent»</a>'
-    render(test)
+    render(`
+      <form>
+        <div class="form-group row">
+            <label for="inputIntelligence" class="col-sm-2 col-form-label">Intelligence:</label>
+            <div class="col-sm-10">
+                <div class="range-slider">
+                    <input class="range-slider__range" type="range" value="50" min="0" max="100">
+                    <span id="value_range1" class="range-slider__value">0</span>
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputIntelligence" class="col-sm-2 col-form-label">Strength:</label>
+            <div class="col-sm-10">
+                <div class="range-slider">
+                    <input class="range-slider__range" type="range" value="50" min="0" max="100">
+                    <span id ="value_range2" class="range-slider__value">0</span>
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputIntelligence" class="col-sm-2 col-form-label">Speed:</label>
+            <div class="col-sm-10">
+                <div class="range-slider">
+                    <input class="range-slider__range" type="range" value="50" min="0" max="100">
+                    <span id="value_range3" class="range-slider__value">0</span>
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputIntelligence" class="col-sm-2 col-form-label">Durability:</label>
+            <div class="col-sm-10">
+                <div class="range-slider">
+                    <input class="range-slider__range" type="range" value="50" min="0" max="100">
+                    <span id="value_range4" class="range-slider__value">0</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-sm-10">
+                <a class="btn btn-success btn-lg" href="/game" role="button">See opponent »</a>
+            </div>
+        </div>
+    </form>
+    <p id="total"></p>`)
   },
   '/opponent' : () => {
     const fakeOpponent = '<p>Your opponent: Hubert de Montmirail</p><a class="btn btn-success btn-lg" href="/game" role="button">Ready to play»</a>'
