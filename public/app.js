45844fb8e68a686b125c6c69db76dff001a032d4
Aurelie Bayre <a.bayre@gmail.com>
2018-04-06 02:39:19 +0200

    feat: ADD js to use inputs

--- public/app.js
+++ public/app.js
@@ -54,6 +54,36 @@ const controllers = {
         </div>
     </form>
     <p id="total"></p>`)
+
+    getTotal = () => {
+      console.log("coucou je suis dans getTotal")
+        const total = document.getElementById("total")
+        const first = document.getElementById("value_range1").innerText
+        const second = document.getElementById("value_range2").innerText
+        const third = document.getElementById("value_range3").innerText
+        const fourth = document.getElementById("value_range4").innerText
+        total.innerHTML = "" + (Number(first) + Number(second) + Number(third) + Number(fourth))
+    }
+    //on récupère la valeur de chaque range-slider
+    const rangeSlider = function () {
+        let slider = $('.range-slider'),
+            range = $('.range-slider__range'),
+            value = $('.range-slider__value');
+
+        slider.each(function () {
+            value.each(function () {
+                const value = $(this).prev().attr('value');
+                $(this).html(value);
+            });
+
+            range.on('input', function () {
+                console.log(this.value)
+                $(this).next(value).html(this.value)
+                getTotal()
+            });
+        });
+    };
+    rangeSlider()
   },
   '/opponent' : () => {
     const fakeOpponent = '<p>Your opponent: Hubert de Montmirail</p><a class="btn btn-success btn-lg" href="/game" role="button">Ready to play»</a>'
